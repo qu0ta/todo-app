d3cc99a46a2ae14a4be84f19d250d1ae23470c84
Stanislaw <qu0tagod@gmail.com>
2023-05-20 23:35:59 +0300

    Edited

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/qu0ta/todo-app"
 	"github.com/qu0ta/todo-app/pkg/handler"
 	"log"
@@ -9,6 +10,7 @@ import (
 func main() {
 	handler := new(handler.Handler)
 	srv := new(todo.Server)
+	fmt.Println("Start!")
 
 	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
 		log.Fatalf("error while running server: %s", err.Error())
