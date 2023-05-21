71297c525d5a1327fdf74c065596c2bdae48abb7
Stanislaw <qu0tagod@gmail.com>
2023-05-22 00:42:15 +0300

    Make connection to database from application

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,20 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs, %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("error create postgres db: %s", err)
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
