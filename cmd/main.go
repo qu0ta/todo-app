11ceb93271c9693a067aeda6a10fea1beda12596
Stanislaw <qu0tagod@gmail.com>
2023-05-21 23:58:54 +0300

    Replace hard-code variable port with env variable using config file.

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,25 @@ import (
 	"github.com/qu0ta/todo-app/pkg/handler"
 	"github.com/qu0ta/todo-app/pkg/repository"
 	"github.com/qu0ta/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs, %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running server: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
