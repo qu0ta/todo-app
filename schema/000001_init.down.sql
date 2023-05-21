fdf670cdb93d35d8ee5a32df8c20c0c3bede7879
Stanislaw <qu0tagod@gmail.com>
2023-05-22 00:23:53 +0300

    Update migrations files for correct work

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,9 +1,5 @@
-DROP TABLE lists_items;
-
-DROP TABLE users_lists;
-
-DROP TABLE todo_lists;
-
-DROP TABLE users;
-
-DROP TABLE todo_items;
+DROP TABLE IF EXISTS users_lists;
+DROP TABLE IF EXISTS lists_items;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS todo_lists;
+DROP TABLE IF EXISTS todo_items;
